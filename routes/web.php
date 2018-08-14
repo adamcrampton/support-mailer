d1b950ece165c9dd0bf314a29e1d87c9de3af3f6
Adam Crampton <adam.crampton@alluremedia.com.au>
2018-08-14 16:20:48 +1000

    Added routes and controllers for CRUD functions

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,10 @@ Route::get('/', 'PageController@index');
 
 // Admin routes.
 Route::resource('admin', 'AdminController');
+
+// CRUD routes.
+Route::resource('config', 'ConfigController');
+Route::resource('issue_types', 'IssueTypeController');
+Route::resource('staff_members', 'StaffMemberController');
+Route::resource('logs', 'SubmissionLogController');
+
