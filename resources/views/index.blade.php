c378ab00857ba8a7e4f04e7274613067dc9da8a7
Adam Crampton <adam.crampton@alluremedia.com.au>
2018-08-15 15:01:38 +1000

    Converted some input fields to use Forms class

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -64,6 +64,7 @@
                         <label class="col-lg-3 col-form-label form-control-label" for="provider_list">Select a provider</label>
                         <div class="col-lg-9">
                           <select class="form-control" name="provider_list" id="provider_list">
+                            <option selected="selected" value>Please select one:</option>
                             @foreach ($providers as $provider)
                               <option value="{{ $provider->id }}">{{ $provider->provider_name }}</option>
                             @endforeach
@@ -71,13 +72,13 @@
                         </div>
                       </div>
                       @endif
-
                       {{-- Display either input fields or staff member select, depending on config option. --}}
                       @if ($config->use_staff_list)
                       <div class="form-group row">
                         <label class="col-lg-3 col-form-label form-control-label" for="staff_list">Select your name (add your name in the Issue Details field if you can't find your name in the list)</label>
                         <div class="col-lg-9">
                           <select class="form-control" name="staff_list[]" id="staff_list">
+                            <option selected="selected" value>Please select one:</option>
                             @foreach ($staffMembers as $staffMember)
                               <option value="[{{$staffMember->id}}, {{ $staffMember->staff_name }}, {{ $staffMember->staff_email }}]">{{ $staffMember->staff_name }}</option>
                             @endforeach
@@ -89,59 +90,51 @@
                       <div class="form-group row">
                           <label class="col-lg-3 col-form-label form-control-label">First name</label>
                           <div class="col-lg-9">
-                              {{-- <input class="form-control" type="text" name="first_name" id="first_name"> --}}
-                              {{ Form::text('first_name') }}
+                              {{ Form::text('first_name', '', ['class' => 'form-control', 'id' => 'first_name']) }}
                           </div>
                       </div>
                       <div class="form-group row">
                           <label class="col-lg-3 col-form-label form-control-label">Last name</label>
                           <div class="col-lg-9">
-                              <input class="form-control" type="text" name="last_name" id="last_name">
+                            {{ Form::text('last_name', '', ['class' => 'form-control', 'id' => 'last_name']) }}
                           </div>
                       </div>
                       <div class="form-group row">
                           <label class="col-lg-3 col-form-label form-control-label">Email</label>
                           <div class="col-lg-9">
-                              <input class="form-control" type="email" name="email" id="email">
+                              {{ Form::email('email', '', ['class' => 'form-control', 'id' => 'email']) }}
                           </div>
                       </div>
                       @endif
-
                       <div class="form-group row">
                         <label class="col-lg-3 col-form-label form-control-label" for="preferred_contact">Preferred contact method</label>
                         <div class="col-lg-9">
-                          <select class="form-control" name="preferred_contact" id="preferred_contact">
-                              <option value="Email">Email</option>
-                              <option value="Phone">Phone</option>
-                          </select>
+                          {{ Form::select('preferred_contact', ['email' => 'Email', 'phone' => 'Phone'], null, ['class' => 'form-control', 'id' => 'form-control', 'placeholder' => 'Choose an option:']) }}
                         </div>
                       </div>
-
                       <div class="form-group row">
                           <label class="col-lg-3 col-form-label form-control-label" for="phone_number">Phone number</label>
                           <div class="col-lg-9">
-                              <input class="form-control" type="text" name="phone_number" id="phone_number">
+                              {{ Form::text('phone_number', '', ['class' => 'form-control', 'id' => 'phone_number']) }}
                           </div>
                       </div>
-
                       <div class="form-group row">
                         <label class="col-lg-3 col-form-label form-control-label" for="issue_type">Select an issue type</label>
                         <div class="col-lg-9">
                           <select class="form-control" name="issue_type" id="issue_type">
+                            <option selected="selected" value>Please select one:</option>
                             @foreach ($issueList as $issueType)
                               <option value="{{ $issueType->id }}">{{ $issueType->issue_name }}</option>
                             @endforeach
                           </select>
                         </div>
                       </div>
-
                       <div class="form-group row">
                        <label class="col-lg-3 col-form-label form-control-label">Issue Details</label>
                           <div class="col-lg-9">
-                              <textarea rows="6" name="issue_details" class="form-control" id="issue_details"></textarea>
+                              {{ Form::textarea('issue_details', '', ['class' => 'form-control', 'id' => 'issue_details', 'rows' => '6']) }}
                           </div>
                       </div>
-
                       <div class="form-group row">
                           <label class="col-lg-3 col-form-label form-control-label"></label>
                           <div class="col-lg-9">
