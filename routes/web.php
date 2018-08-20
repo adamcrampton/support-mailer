73436d56136cc3e0a81e4c6a8c8a906b3dd7e880
Adam Crampton <adam.crampton@alluremedia.com.au>
2018-08-20 18:12:40 +1000

    Added batch update routes for other controllers

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,14 @@ Route::resource('admin', 'AdminController');
 // CRUD routes.
 Route::resource('config', 'ConfigController');
 Route::resource('issue_types', 'IssueTypeController');
-Route::post('/batch_update', 'IssueTypeController@batchUpdate');
+Route::post('/issue_type_batch_update', 'IssueTypeController@batchUpdate');
 Route::resource('providers', 'ProviderController');
+Route::post('/provider_batch_update', 'ProviderController@batchUpdate');
 Route::resource('staff_members', 'StaffMemberController');
-Route::resource('logs', 'SubmissionLogController');
+Route::post('/staff_member_batch_update', 'StaffMemberTypeController@batchUpdate');
 Route::resource('users', 'UserController');
+Route::post('/batch_update', 'UserController@batchUpdate');
+Route::resource('logs', 'SubmissionLogController');
 
 // Auth Routes.
 Auth::routes();
